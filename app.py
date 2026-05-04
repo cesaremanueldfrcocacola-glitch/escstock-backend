ce4ab13c23501e54aeeb662cb9f185af98485219
cesaremanueldfrcocacola-glitch <cesaremanueldfrcocacola@gmail.com>
2026-05-03 21:58:32 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+from database import engine
+from models import Base, Producto
+from routes import router
+
+# Crear tablas si no existen
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# Permitir acceso desde la APP Android
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Ruta raíz
+@app.get("/")
+def home():
+    return {"status": "ESCStock Online"}
+
+# Agregar rutas del sistema
+app.include_router(router)
